f422108745582ed3c05f55441e4e422588dfcb81
darthpasta <liowj.nick@gmail.com>
2018-07-12 15:48:56 +0800

    add post list component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,35 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatInputModule, MatCardModule } from '@angular/material';
+import { 
+  MatInputModule, 
+  MatCardModule, 
+  MatButtonModule,
+  MatToolbarModule,
+  MatExpansionModule
+ } from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 import { PostCreateComponent } from './posts/post-create/post-create.component';
-
+import { PostListComponent } from './posts/post-list/post-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    PostCreateComponent
+    HeaderComponent,
+    PostCreateComponent,
+    PostListComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
     MatInputModule,
-    MatCardModule
+    MatCardModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatExpansionModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts/post-list/post-list.component.ts
@@ -0,0 +1,24 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'app-post-list',
+    templateUrl: './post-list.component.html',
+    styleUrls: ['./post-list.component.css']
+})
+
+export class PostListComponent {
+    posts = [
+        {
+            title: 'First Post',
+            content: 'This is the content of first post'
+        },
+        {
+            title: 'Second Post',
+            content: 'This is the content of second post'
+        },
+        {
+            title: 'Third Post',
+            content: 'This is the content of third post'
+        }
+    ]
+}
