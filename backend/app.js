d3ce47446ca133582325c340aae34f5eecb9837e
darthpasta <liowj.nick@gmail.com>
2018-07-14 13:43:35 +0800

    Will now successfully add data to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,21 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
 
 // returns us an express app, big chain of middle ware
 const app = express();
+
+// connect to mongodb
+// connect method will return a promise
+mongoose.connect('mongodb+srv://Nick:PfzELEQ3Lo873dUa@cluster0-3swpg.mongodb.net/mean?retryWrites=true',  { useNewUrlParser: true })
+    .then(() => {
+        console.log('Connected to database');
+    })
+    .catch(() => {
+        console.logo('Connection failed');
+    })
 
 app.use(bodyParser.json());
 
@@ -23,8 +36,17 @@ app.use((req, res, next) => {
 // triggers middleware when there is a post request
 app.post("/api/posts", (req, res, next) => {
     // post request body has data
-    const post = req.body;
+    const post = new Post({
+        title: req.body.title,
+        content: req.body.content
+    });
+
+    // every model created with mongoose has a save function
+    // collection name is the plural of models
+    post.save();
+
     console.log(post);
+    
     // 201 success with added resources
     // after success show json message
     res.status(201).json({
---
+++ backend/models/post.js
@@ -0,0 +1,14 @@
+// 3rd party app that allows schema creation (creates structure) for unstructured data
+const mongoose = require('mongoose');
+
+// creates blueprint of how your data would look like
+const postSchema = mongoose.Schema({
+    // learn more on mongoose docs
+    title: { type: String, required: true },
+    content: { type: String, required: true }
+});
+
+// schema is not the model yet
+// use the model function to turn the blueprint into a model 
+// first argument is model name, second argument is name of blueprint
+module.exports =  mongoose.model('Post', postSchema);
