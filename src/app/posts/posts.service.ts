b8d2912caab4c30fa05e877cc023df3d58b19d1d
darthpasta <liowj.nick@gmail.com>
2018-07-19 14:54:40 +0800

    Can now edit post

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -51,7 +51,7 @@ export class PostsService {
         // find post with same id and put them in a new js object
         // return {...this.posts.find((p) => p.id === id)};
 
-        return this.http.get<{_id: string, title: string, content: string}>("http://localhost:3000/api/posts/" + id);
+        return this.http.get<{_id: string, title: string, content: string, imagePath: string}>("http://localhost:3000/api/posts/" + id);
     }
 
     addPost(title: string, content: string, image: File) {
@@ -82,19 +82,43 @@ export class PostsService {
 
     }
 
-    updatePost(id:string, title: string, content: string) {
-        const post: Post = {
-            id: id,
-            title: title,
-            content: content,
-            imagePath: null
+    updatePost(id:string, title: string, content: string, image: File | string) {
+        // const post: Post = {
+        //     id: id,
+        //     title: title,
+        //     content: content,
+        //     imagePath: null
+        // }
+        let postData: Post | FormData;
+        if (typeof(image) === 'object') {
+            // create form data object
+            postData = new FormData();
+            postData.append("id", id);
+            postData.append("title", title);
+            postData.append("content", content);
+            postData.append("image", image, title);
+
+        } else {
+            // string -- send normal json data
+            postData = {
+                id: id, 
+                title: title,
+                content: content,
+                imagePath: image
+            };
         }
 
         this.http
-            .put("http://localhost:3000/api/posts/" + id, post)
+            .put("http://localhost:3000/api/posts/" + id, postData)
             .subscribe(response => {
                 const updatedPosts = [...this.posts];
-                const oldPostIndex = updatedPosts.findIndex(p => p.id === post.id);
+                const oldPostIndex = updatedPosts.findIndex(p => p.id === id);
+                const post: Post = {
+                    id: id,
+                    title: title,
+                    content: content, 
+                    imagePath: "response.imagePath"
+                }
                 updatedPosts[oldPostIndex] = post;
                 this.posts = updatedPosts;
                 this.postsUpdated.next([...this.posts]);
